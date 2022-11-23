19f69978468d7f18545b01f0480e54ab1b6aa76a
Dakota Lee Martinez <dakotaleemusic@gmail.com>
2022-11-23 14:06:20 -0800

    add solution to lesson 3

--- 03_DOM_Events/src/index.js
+++ 03_DOM_Events/src/index.js
@@ -1,3 +1,4 @@
+
 function formatPrice(price) {
   // return `$${price.toFixed(2)}`;
   return '$' + Number.parseFloat(price).toFixed(2);
@@ -50,12 +51,80 @@ function renderBook(book) {
   const btn = document.createElement('button');
 
   btn.textContent = 'Delete';
+  btn.addEventListener('click', (e) => {
+    // e.target.parentNode.remove()
+    li.remove()
+  })
 
   li.append(h3, pAuthor, pPrice, img, btn);
 
   document.querySelector('#book-list').append(li);
 }
 
+///////////////////
+// Event Handlers
+///////////////////
+
+const toggleFormButton = document.querySelector('#toggleForm');
+const newBookForm = document.querySelector('#book-form');
+let bookFormVisible = false;
+
+// hide and show the new book form when toggle buton is clicked
+toggleFormButton.addEventListener('click', (e) => {
+  bookFormVisible = !bookFormVisible;
+  newBookForm.classList.toggle('collapsed');
+  toggleFormButton.textContent = bookFormVisible ? 'Hide Book Form' : 'New Book';
+  // equivalent to the below
+  // if (bookFormVisible) {
+  //   toggleFormButton.textContent = 'Hide Book Form';
+  // } else {
+  //   toggleFormButton.textContent = 'New Book';
+  // }
+});
+
+// also hide the form when it's visible and the escape key is pressed
+
+window.addEventListener('keydown', (e) => {
+  console.log(e)
+  if (e.key === "Escape" && bookFormVisible) {
+    bookFormVisible = !bookFormVisible;
+    newBookForm.classList.toggle('collapsed');
+    toggleFormButton.textContent = bookFormVisible ? 'Hide Book Form' : 'New Book';
+  }
+})
+
+// handle submitting new book form
+newBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // invoke renderBook to add the book data from the form into the DOM
+  // renderBook expects a book object as an argument, so we need to build that
+  // book should look something like this:
+  // {
+  //   id:1,
+  //   title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+  //   author: 'Marjin Haverbeke',
+  //   price: 10.00,
+  //   reviews: [{userID: 1, content:'Good book, but not great for new coders'}],
+  //   inventory: 10,
+  //   imageUrl: 'https://images-na.ssl-images-amazon.com/images/I/51IKycqTPUL._SX218_BO1,204,203,200_QL40_FMwebp_.jpg',
+  // }
+  console.dir(e.target)
+  const book = {
+    title: e.target.title.value,
+    author: e.target.author.value,
+    price: Number.parseFloat(e.target.price.value),
+    reviews: [],
+    inventory: Number.parseInt(e.target.inventory.value),
+    imageUrl: e.target.imageUrl.value
+  }
+  renderBook(book);
+});
+
+const langSelect = document.querySelector('#form-language')
+langSelect.addEventListener('change', (e) => {
+  console.log(e.target.value);
+})
 
 ////////////////////////////////////////////
 // call render functions to populate the DOM
@@ -64,4 +133,4 @@ function renderBook(book) {
 renderHeader(bookStore);
 renderFooter(bookStore);
 bookStore.inventory.forEach(renderBook);
-
+  
