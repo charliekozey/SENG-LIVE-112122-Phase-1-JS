422147641f55280adc2e09a579f7c3d08fbf4edb
charliekozey <charkoz@mailbox.org>
2023-02-28 22:54:40 -0500

    add favorite toggle to cookies

--- 06_PATCH_and_DELETE_Requests/src/cookie-crud.js
+++ 06_PATCH_and_DELETE_Requests/src/cookie-crud.js
@@ -4,24 +4,27 @@ function fetchCookies() {
         .then(cookies => cookies.forEach(cookie => renderCookie(cookie)))
 }
 
-function renderCookie(cookie) {
+function renderCookie(cookie) {    
     const cookieMenu = document.getElementById("cookie-menu")
     const cookieCard = document.createElement("div")
     const cookieName = document.createElement("h1")
-    const cookieDeliciousness = document.createElement("h2")
+    const cookieQuantity = document.createElement("h2")
     const cookieImage = document.createElement("img")
     const deleteBtn = document.createElement("button")
+    const favoriteBtn = document.createElement("button")
 
     cookieName.textContent = cookie.name
-    cookieDeliciousness.textContent = `Deliciousness: ${cookie.deliciousness}/5`
+    cookieQuantity.textContent = `Cookies in stash: ${cookie.quantity}`
     cookieImage.src = cookie.image_url
     cookieCard.id = "cookie-card"
-    deleteBtn.textContent = "EAT"
+    deleteBtn.textContent = "EAT ALL"
+    favoriteBtn.textContent = cookie.isFavorite ? "★" : "☆"
 
     deleteBtn.addEventListener("click", () => deleteCookie(cookie, cookieCard))
+    favoriteBtn.addEventListener("click", () => toggleFavorite(cookie))
 
-    cookieCard.append(cookieName, cookieDeliciousness, cookieImage, deleteBtn)
-    cookieMenu.append(cookieCard)   
+    cookieCard.append(cookieName, cookieQuantity, cookieImage, deleteBtn, favoriteBtn)
+    cookieMenu.append(cookieCard)
 }
 
 function deleteCookie(cookie, cookieCard) {
@@ -29,6 +32,22 @@ function deleteCookie(cookie, cookieCard) {
         method: "DELETE"
     })
         .then(cookieCard.remove())
+}
+
+function toggleFavorite(cookie) {
+    fetch(`http://localhost:3000/cookies/${cookie.id}`, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({"isFavorite": !cookie.isFavorite})
+    })
+        .then(res => res.json())
+        .then(cookie => updateCookie(cookie))
+}
+
+function updateCookie(cookie) {
+    
 }
 
 fetchCookies()
