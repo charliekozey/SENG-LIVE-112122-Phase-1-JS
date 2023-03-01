8b0489c0a5e4eb0394cd3f69ffc20fd0dad939ae
charliekozey <charkoz@mailbox.org>
2023-03-01 00:03:47 -0500

    add patch favorite functionality

--- 06_PATCH_and_DELETE_Requests/src/cookie-crud.js
+++ 06_PATCH_and_DELETE_Requests/src/cookie-crud.js
@@ -6,48 +6,51 @@ function fetchCookies() {
 
 function renderCookie(cookie) {    
     const cookieMenu = document.getElementById("cookie-menu")
+    
     const cookieCard = document.createElement("div")
-    const cookieName = document.createElement("h1")
-    const cookieQuantity = document.createElement("h2")
-    const cookieImage = document.createElement("img")
-    const deleteBtn = document.createElement("button")
-    const favoriteBtn = document.createElement("button")
-
-    cookieName.textContent = cookie.name
-    cookieQuantity.textContent = `Cookies in stash: ${cookie.quantity}`
-    cookieImage.src = cookie.image_url
     cookieCard.id = "cookie-card"
+    
+    const cookieName = document.createElement("h1")
+    cookieName.textContent = cookie.name
+    
+    const cookieQuantity = document.createElement("h2")
+    cookieQuantity.textContent = `Cookies in stash: ${cookie.quantity}`
+    
+    const cookieImage = document.createElement("img")
+    cookieImage.src = cookie.image_url
+    
+    const deleteBtn = document.createElement("button")
     deleteBtn.textContent = "EAT ALL"
-    favoriteBtn.textContent = cookie.isFavorite ? "★" : "☆"
-
     deleteBtn.addEventListener("click", () => deleteCookie(cookie, cookieCard))
-    favoriteBtn.addEventListener("click", () => toggleFavorite(cookie))
+    
+    const favoriteBtn = document.createElement("button")
+    let favoriteToggle = cookie.isFavorite
+    favoriteBtn.textContent = cookie.isFavorite ? "★" : "☆"
+    favoriteBtn.addEventListener("click", (e) => {
+        favoriteToggle = !favoriteToggle
+        favoriteBtn.textContent = favoriteToggle ? "★" : "☆"
+        updateFavorite(cookie, favoriteToggle)
+    })
 
     cookieCard.append(cookieName, cookieQuantity, cookieImage, deleteBtn, favoriteBtn)
     cookieMenu.append(cookieCard)
-}
+}   
 
 function deleteCookie(cookie, cookieCard) {
     fetch(`http://localhost:3000/cookies/${cookie.id}`, {
         method: "DELETE"
-    })
+    })  
         .then(cookieCard.remove())
 }
 
-function toggleFavorite(cookie) {
+function updateFavorite(cookie, favoriteToggle) {
     fetch(`http://localhost:3000/cookies/${cookie.id}`, {
         method: "PATCH",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({"isFavorite": !cookie.isFavorite})
+        body: JSON.stringify({"isFavorite": favoriteToggle})
     })
-        .then(res => res.json())
-        .then(cookie => updateCookie(cookie))
-}
-
-function updateCookie(cookie) {
-    
 }
 
 fetchCookies()
